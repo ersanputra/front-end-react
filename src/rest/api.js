47aea64cbe8d98493ca8c8860a6c41824e9e23d7
ersanputra <ersancakra@gmail.com>
2023-11-19 08:45:32 +0700

    update integrasi cakes

--- src/rest/api.js
+++ src/rest/api.js
@@ -41,3 +41,11 @@ export const postRegistrtion = async ({
   }
 }
 
+export const getCakes = async () => {
+    try {
+        const response = await api.get('/api/cakes');
+        return response.data.data;
+    } catch (error) {
+        throw error;
+    }
+}
