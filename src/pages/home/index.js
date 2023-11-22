8cadef80f1557e6a3885fdba191feca524c62c1a
ersancakra <ersancakra@gmail.com>
2023-11-22 10:05:49 +0700

    push

--- src/pages/home/index.js
+++ src/pages/home/index.js
@@ -1,20 +1,49 @@
-import React, { useState } from "react";
-import Navbar from "@/components/Navbar";
-import Hero from "@/components/Hero";
-import HeadlineCards from "@/components/HeadlineCards";
-import Food from "@/components/Food";
+// pages/home/index.js
 
+import React, { useState } from 'react';
+import Navbar from '@/components/Navbar';
+import Hero from '@/components/Hero';
+import HeadlineCards from '@/components/HeadlineCards';
+import Food from '@/components/Food';
+import ShoppingCart from '@/components/ShoppingCart';
 
-export default function Main() {
+export default function Home() {
   const [isCartOpen, setIsCartOpen] = useState(false);
+  const [cart, setCart] = useState([]);
+
+  const addToCart = (product) => {
+
+    const existingProduct = cart.find((item) => item.id === product.id);
+
+    if (existingProduct) {
+      setCart((prevCart) =>
+        prevCart.map((item) =>
+          item.id === product.id ? { ...item, quantity: item.quantity + 1 } : item
+        )
+      );
+    } else {
+
+      setCart((prevCart) => [...prevCart, { ...product, quantity: 1 }]);
+    }
+  };
+
+  const updateCartItemCount = (count) => {
+
+  };
 
   return (
     <div>
-      <Navbar />
+      <Navbar toggleCart={() => setIsCartOpen(!isCartOpen)} />
       <Hero />
       <HeadlineCards />
-      <Food />
-      
+      <Food addToCart={addToCart} />
+
+      {isCartOpen && (
+        <ShoppingCart
+          updateCartItemCount={updateCartItemCount}
+          data={cart}
+        />
+      )}
     </div>
   );
 }
